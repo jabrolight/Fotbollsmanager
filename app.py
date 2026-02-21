a4bb70dbbc2186349d498418d0c49097d32f60ad
jabrolight <peterjabro2003@gmail.com>
2026-02-21 22:23:54 +0100

    first two templates created

--- app.py
+++ app.py
@@ -1,17 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template
 from backend import Liga
+
 
 app = Flask(__name__)
 
 premier_league = Liga("Lagen.db")
 
 @app.route("/")
-def start_sida():
-    premier_league.fylla_dict_med_objekt()
-    alla_lag = premier_league.alla_lagnamn()
-    
-    # 2. Servera datan (Webbläsare förstår enkla HTML-taggar som <h1>)
-    return f"<h1>Min Fotbollsliga</h1><p>Lagen i databasen är: {alla_lag}</p>"
+def index():
+    return render_template("index.html")
+
+@app.route("/demo")
+def demo():
+    return render_template("demo.html")
 
 # Detta startar själva webbservern
 if __name__ == "__main__":
